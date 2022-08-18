458e7bed93b514108d94d2d288086fb2d42a9746
Santiago Rodriguez <rodriguez.ivan.santiago8@gmail.com>
2022-08-18 20:30:19 -0300

    Clase 03

---
+++ Clase-03/src/app/models/producto.model.ts
@@ -0,0 +1,10 @@
+export class ProductoModel {
+    constructor(
+        public id: number,
+        public codigo: number,
+        public nombre: string,
+        public fecha: Date
+    ) {
+
+    }
+}
