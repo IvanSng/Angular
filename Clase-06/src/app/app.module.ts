ce31c3bf9f0a41212d744f09be152c69c5e989bb
Santiago Rodriguez <rodriguez.ivan.santiago8@gmail.com>
2022-09-02 18:36:18 -0300

    Clase 06

---
+++ Clase-06/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CabeceraComponent } from './components/cabecera/cabecera.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabeceraComponent,
+    ServiciosComponent,
+    AboutComponent,
+    ContactoComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Clase-06/src/app/components/cabecera/cabecera.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecera',
+  templateUrl: './cabecera.component.html',
+  styleUrls: ['./cabecera.component.css']
+})
+export class CabeceraComponent implements OnInit {
+
+  @Input()
+  titulo:string = "<Titulo por defecto>";
+  angulo: number = 0;
+  constructor() { }
+
+  ngOnInit(): void {
+    setInterval(()=> {
+      this.angulo++
+    }, 30)
+  }
+
+}
